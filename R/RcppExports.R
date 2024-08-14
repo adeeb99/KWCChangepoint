2e1b9848fd7ab86e969c2dcb9247c62a06e5c2cc
adeeb99 <adeeb.rouhani@gmail.com>
2024-08-14 00:50:00 -0400

    Fixed Exports

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,26 +2,26 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 mySeq <- function(s, e) {
-    .Call(`_ChangepointDetection_mySeq`, s, e)
+    .Call(`_KWCChangepoint_mySeq`, s, e)
 }
 
 cost_cpp <- function(s, e, data) {
-    .Call(`_ChangepointDetection_cost_cpp`, s, e, data)
+    .Call(`_KWCChangepoint_cost_cpp`, s, e, data)
 }
 
 updateR <- function(R1, obj, F_tau_star, beta, K, tau_star) {
-    .Call(`_ChangepointDetection_updateR`, R1, obj, F_tau_star, beta, K, tau_star)
+    .Call(`_KWCChangepoint_updateR`, R1, obj, F_tau_star, beta, K, tau_star)
 }
 
 PELT <- function(data, N, beta, K = 0) {
-    .Call(`_ChangepointDetection_PELT`, data, N, beta, K)
+    .Call(`_KWCChangepoint_PELT`, data, N, beta, K)
 }
 
 getOnes <- function(vec) {
-    .Call(`_ChangepointDetection_getOnes`, vec)
+    .Call(`_KWCChangepoint_getOnes`, vec)
 }
 
 PELT_NL <- function(data, N, c2 = 1, m0 = 1L) {
-    .Call(`_ChangepointDetection_PELT_NL`, data, N, c2, m0)
+    .Call(`_KWCChangepoint_PELT_NL`, data, N, c2, m0)
 }
 
